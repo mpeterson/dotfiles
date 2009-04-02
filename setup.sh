ca81292fe5095155e17acd0ad753c3a972214e2f
Michel Peterson <michel@peterson.com.ar>
2009-04-02 19:00:23 -0300

    Added a setup file
    This will aid in the creation of the needed symlinks

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+cd $(dirname $0)
+VC_DIR=$(pwd)
+find . -iname ".*" -type f -o -type d -not -name "$(basename .)" -not -name .git -maxdepth 1 | \
+    while read path; do
+        if [ "X$(basename "$path")" = "X.git" ]; then
+            continue
+        fi
+        destination="$HOME/$path"
+        if [ -e "$destination" ]; then
+            if [ -L "$destination" ]; then
+                echo "Symlink for $path exists, skipping."
+                continue
+            else
+                echo "backing up existing $destination to ${destination}.vc_backup"
+                mv "$destination" "${destination}.vc_backup"
+            fi
+        fi
+        echo "doing symlink for $path"
+        ln -s "$VC_DIR/$path" "$HOME/$path"
+    done
