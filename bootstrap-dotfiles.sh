c44374ea4fb42cf1471a35c1732c20028ee4dd83
Michel Peterson <michel@peterson.com.ar>
2020-06-26 16:29:40 +0300

    Fix installation script

--- bootstrap-dotfiles.sh
+++ bootstrap-dotfiles.sh
@@ -83,7 +83,7 @@ if [ $? = 0 ]; then
   echo "Checked out config."
 else
   echo "Backing up pre-existing dot files."
-  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+  dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
 fi
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
