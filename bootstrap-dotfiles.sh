6e44629a84320e2bc227a8c0b8532a7e76372e2c
Michel Peterson <michel@peterson.com.ar>
2024-11-05 20:48:42 +0200

    Update bootstrap-dotfiles.sh

--- bootstrap-dotfiles.sh
+++ bootstrap-dotfiles.sh
@@ -66,7 +66,8 @@ if [ -n "$apt" ]; then
 elif [ -n "$dnf" ]; then
   $INSTALL epel-release
   $INSTALL neovim
-  python3 -m pip install --user --upgrade --break-system-packages pynvim typing_extensions
+  $INSTALL pip
+  python3 -m pip install --user --upgrade pynvim typing_extensions
 elif [ -n "$brew" ]; then
   $INSTALL neovim
   python3 -m pip install --user --upgrade --break-system-packages pynvim typing_extensions
